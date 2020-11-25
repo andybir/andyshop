ee4fcb8429e79ddf24eee6cbc34ffe1707c78fca
Andy Birosak <andyb@Andys-MacBook-Pro.local>
2020-11-25 16:40:36 -0500

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'iPhone 11 Pro 256GB Memory',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'ASUS Maximus VIII Hero Motherboard',
+    image: '/images/maximus-mobo.jpeg',
+    description:
+      'LGA 1151 Socket for 6th Generation Intel core Desktop Processor. SupremeFX 2015. USB 3.1 Type A & C Onboard. Sonic Sense Amp, Lighting Control. NOTE: Update the motherboard to latest firmware/BIOS version to "3401" for best result.',
+    brand: 'ASUS',
+    category: 'PC Parts',
+    price: 199.99,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    name: 'EVGA GeForce GTX 980 Ti',
+    image: '/images/evga-gpu-front.jpeg',
+    description:
+      "Virtual Reality Ready. Great for 4K (GeForce GTX 980 Ti & above). DirectX12 Ready. Gamestream to NVIDIA SHIELD. EVGA 'ACX 2.0+' Cooling Technology - Fine-Tuned to Perfection. EVGA's 24/7 Technical Support; Base Clock: 1102 MHz / Boost Clock: 1190 MHz. Memory Clock: 7010 MHz Effective; CUDA Cores: 2816; Memory Detail: 6144MB GDDR5. Recommended PSU: 600W or greater power supply",
+    brand: 'EVGA',
+    category: 'PC Parts',
+    price: 499.99,
+    countInStock: 1,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Welcome to AndyShop',
+  description: 'Used PC parts and electronics',
+  keywords: 'pc parts, electronics, used',
+}
+
+export default Meta
